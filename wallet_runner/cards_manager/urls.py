b93b3431a1bfe734d770d7f70db764869dfa47a8
stlad <82213506+stlad@users.noreply.github.com>
2022-04-07 21:09:44 +0500

    upd

--- wallet_runner/cards_manager/urls.py
+++ wallet_runner/cards_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     #path('find/', views.find),
-    re_path(r'^find/(?P<x>[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)/(?P<y>[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)/', views.get_stores),
+    re_path(r'^find/(?P<x>[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)/(?P<y>[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)/', views.get_stores_by_url),
     # регулярное выражения имеет в виду любое действительное число
 ]
--- wallet_runner/cards_manager/views.py
+++ wallet_runner/cards_manager/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Areas
 from .db_controller import *
+from cards_manager import db_controller
 # Create your views here.
 
 def index(request):
     return HttpResponse("e")
 
-def get_stores(reques,x,y):
-    out = f"{x}    {y}"
+def get_stores_by_url(reques,x,y):
+    #out = f"{x}    {y}"
 
-    return HttpResponse(out)
+    return HttpResponse(db_controller.find_area(x,y))
