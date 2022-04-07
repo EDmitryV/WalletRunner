7919aee62c0fef6476c80613e0efca098b2f2fde
stlad <82213506+stlad@users.noreply.github.com>
2022-04-07 20:22:06 +0500

    Работа с адресацией и передачей аргументов в ссылке (поможет для формирования запросов)

--- wallet_runner/cards_manager/urls.py
+++ wallet_runner/cards_manager/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    #path('', views.index),
+    path('', views.index),
+    #path('find/', views.find),
+    re_path(r'^find/(?P<x>[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)/(?P<y>[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)/', views.get_stores),
+    # регулярное выражения имеет в виду любое действительное число
 ]
--- wallet_runner/cards_manager/views.py
+++ wallet_runner/cards_manager/views.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Areas
+from .db_controller import *
 # Create your views here.
+
+def index(request):
+    return HttpResponse("e")
+
+def get_stores(reques,x,y):
+    out = f"{x}    {y}"
+
+    return HttpResponse(out)
