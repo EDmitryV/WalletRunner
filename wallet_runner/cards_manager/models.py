ef758a60bf9dcffe61944a6423497b0bbb10ddbb
stlad <82213506+stlad@users.noreply.github.com>
2022-04-15 00:45:13 +0500

    переименование глупости

--- wallet_runner/cards_manager/models.py
+++ wallet_runner/cards_manager/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Areas(models.Model):
     Area_id = models.IntegerField(primary_key= True)
-    Center_atitude = models.FloatField(default = 0)
+    Center_latitude = models.FloatField(default = 0)
     Center_longitude = models.FloatField(default= 0)
     Radius = models.FloatField()
     Creation_date = models.DateField()
@@ -24,7 +24,7 @@ class Networks(models.Model):
 
 class Stores(models.Model):
     Store_id = models.AutoField(primary_key= True)
-    Store_atitude = models.FloatField(default= 0)
+    Store_latitude = models.FloatField(default= 0)
     Store_longitude = models.FloatField(default= 0)
     Network_id = models.ForeignKey(Networks, on_delete=models.DO_NOTHING)
 
