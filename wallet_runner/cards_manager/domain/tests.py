dfce2d693e2a6934ac4f2bdb72786bb2ec1d545f
EDmitryBJ <edv.busines@gmail.com>
2022-04-22 10:55:55 +0500

    для проверки

--- wallet_runner/cards_manager/domain/tests.py
+++ wallet_runner/cards_manager/domain/tests.py
@@ -1,5 +1,5 @@
 import requests
 
-coordsAndCards = {'latitude': '57.916396', 'longitude': '59.973656', 'cards': ['Монетка', 'Пятёрочка', 'Магнит']}
+coordsAndCards = {'latitude': '60.616477', 'longitude': '56.791952', 'cards': ['Монетка', 'Пятёрочка', 'Магнит']}
 r = requests.get('http://127.0.0.1:8000/SortMyCards', params=coordsAndCards)
 r.text
